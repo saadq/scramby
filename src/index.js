65aa2ed3c681217c730fb9eba15ac75ae62034d6
Saad Quadri <saad@saadquadri.com>
2017-07-10 20:01:22 -0400

    change API to just return the scrambler

--- src/index.js
+++ src/index.js
@@ -11,39 +11,31 @@ const scramblers = {
   '777': require('./scramblers/NNN')['777']
 }
 
-type Opts = {
-  count?: number,
-  type?: '222' | '333' | '444' | '555' | '666' | '777',
-  draw?: { el: HTMLElement, width: number, height: number }
+type Scramble = {
+  state: string,
+  scrambleString: string
 }
 
-type Scramble = string | Array<string>
+type Scrambler = {
+  initialize: () => void,
+  getRandomScramble: () => Scramble,
+  drawScramble: (
+    el: HTMLElement,
+    state: string,
+    width: number,
+    height: number
+  ) => any
+}
 
-/**
- * Generates scrambles for various puzzles.
- *
- * @param [count=1] The amount of scrambles to generate.
- * @param [type='333'] The type of puzzle to generate scramble for.
- * @param [draw] The element to draw the scramble to.
- * @param [dim] The dimensions to set the drawn scramble to.
- *
- * @return {String|Array<String>} A string or an array of strings containing the scramble(s).
- */
+type Puzzle = '222' | '333' | '444' | '555' | '666' | '777'
 
-function scramby({ type = '333', count = 1, draw }: Opts = {}): Scramble {
-  if (!scramblers[type]) {
-    throw new Error(`There is no scrambler for ${type}`)
+function scramby(puzzle: Puzzle = '333'): Scrambler {
+  if (!scramblers[puzzle]) {
+    throw new Error(`There is no scrambler for ${puzzle}`)
   }
 
-  const scrambler = scramblers[type]
-  const { state, scrambleString } = scrambler.getRandomScramble()
-
-  if (draw) {
-    const { el, width, height } = draw
-    scrambler.drawScramble(el, state, width, height)
-  }
-
-  return scrambleString
+  const scrambler: Scrambler = scramblers[puzzle]
+  return scrambler
 }
 
 module.exports = scramby
